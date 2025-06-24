6a321066f754ce9d412235c3d9f3c5ca6f968b64
saef1eldin <127389003+saef1eldin@users.noreply.github.com>
2025-06-24 20:02:45 +0300

    Update vite.config.js

--- document-analytics-frontend/vite.config.js
+++ document-analytics-frontend/vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
   }
-})
+});
