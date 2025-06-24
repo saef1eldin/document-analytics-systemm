ed4ac2e6ce6e53a9a8ab393dfbe5af143031fb4b
saef1eldin <127389003+saef1eldin@users.noreply.github.com>
2025-06-24 19:57:06 +0300

    Update vite.config.js

--- document-analytics-frontend/vite.config.js
+++ document-analytics-frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  }
 })
